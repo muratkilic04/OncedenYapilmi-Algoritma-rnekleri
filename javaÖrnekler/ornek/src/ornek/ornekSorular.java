cd3ff02261c771c210d696efc489c15c90e5b578
Murat Kılıç <158284118+muratkilic04@users.noreply.github.com>
2025-03-28 20:45:59 +0300

    Add files via upload

---
+++ javaÖrnekler/ornek/src/ornek/ornekSorular.java
@@ -0,0 +1,57 @@
+package ornek;
+
+import java.util.Scanner;
+
+public class ornekSorular {
+
+	public static void main(String[] args) {
+		
+		//Birden fazla algoritma örnekleri bu dosya içindedir
+		
+		Scanner s=new Scanner(System.in);
+		
+		//harmonik ortalamayı hesaplayan java programı
+		
+		
+		/*double ort=0.0,sayi=0.0;
+		int n=0;
+		
+		do {
+			System.out.print("Harmonik ortalaması alınacak sayılatrı girin, çıkmak için sıfır(0) girin:");
+			sayi=s.nextDouble();
+			
+			if(sayi!=0) {
+				ort+=1.0/sayi;
+				n++;
+			}
+		}while(sayi!=0);
+		ort=((double)n)/ort;
+		System.out.println("Harmonik ortalama:"+ort);*/
+		
+		
+		
+		
+		//üs hesaplama
+		
+		/*int taban,kuvvet,sonuc=1;
+		
+		System.out.println("Taban:");
+		taban=s.nextInt();
+		
+		System.out.println("Kuvvet:");
+		kuvvet=s.nextInt();
+		
+		for(int i=1;i<=kuvvet;i++) {
+			sonuc*=taban;
+		}
+		System.out.println(taban + " üssü 5" + kuvvet + ":" + sonuc);
+		*/
+		
+		
+	
+		
+		
+	}
+	
+
+}
